08357361ce0599b9aab6c4c7e0322b3371a7767b
HanJeongSeol <hanjeong94@naver.com>
2023-03-16 16:51:17 +0900

    서버 통신 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState('null');
+
+  useEffect(() => {
+    axios.get('http://localhost:3306/api').then((res) => {
+      console.log(res.data.title);
+      setTitle(() => res.data.title);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{title}</h1>
       </header>
     </div>
   );
